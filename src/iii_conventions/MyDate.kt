8a8fa3dd7e1b8787a3b8896b73f10fd2933aeaf0
John Jesus <john.jesus@raytheon.com>
2018-02-19 14:31:22 -0600

    Koan 25-26 completed.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,56 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+// JJJ: extend data class to be Comparable
+//
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+
+    // JJJ: If years are unequal, that's all you need to know
+    //      If same year, then, check month
+    //      If same month, then, check day of month
+    //
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
+// JJJ: This was provided
+//
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+// JJJ: To implement a range, need 'contains' and 'iterator'
+//      The iterator is implemented as a separate class
+//
+class DateRange(override val start: MyDate, override val endInclusive: MyDate):
+        ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun contains(value: MyDate): Boolean {
+        return start <= value && value <= endInclusive
+    }
+
+    override fun iterator(): Iterator<MyDate> = DateRangeIterator(this)
+}
+
+// JJJ: To implement iterator, override hasNext() and next()
+//
+class DateRangeIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current = dateRange.start;
+
+    override fun hasNext(): Boolean {
+        return current <= dateRange.endInclusive
+    }
+
+    override fun next(): MyDate {
+        val result = current
+
+        // The nextDay() method was provided as a utility
+        current = current.nextDay()
+        return result
+    }
+}
